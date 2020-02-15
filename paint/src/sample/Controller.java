9f76a8ea58735c0949cdcc031fdfe8c590de3067
chudopal <chudopal.00@mail.ru>
2020-02-15 13:27:45 +0300

    Draw the rectangle and the oval

--- paint/src/sample/Controller.java
+++ paint/src/sample/Controller.java
@@ -14,6 +14,7 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.WritableImage;
@@ -26,11 +27,31 @@ public class Controller {
     private ColorPicker colorPicker;
     @FXML
     private TextField brushSize;
+
     @FXML
-    private CheckBox eraser;
+    private RadioButton radio_pencil;
+
+    @FXML
+    private RadioButton radio_erase;
+
+    @FXML
+    private RadioButton radio_line;
+
+    @FXML
+    private RadioButton radio_rect;
+
+    @FXML
+    private RadioButton radio_oval;
+
+    private double begin_x = 0;
+    private double begin_y = 0;
+    private double end_x = 0;
+    private double end_y = 0;
+    private boolean flag = false;
 
     public Controller() {
     }
+
 
     public void initialize(){
         GraphicsContext g = canvas.getGraphicsContext2D();
@@ -40,11 +61,54 @@ public class Controller {
             double x = e.getX() - size / 2;
             double y = e.getY() - size / 2;
 
-            if (eraser.isSelected()){
-                g.clearRect(x, y, size, size);
-            } else {
+
+            if(radio_pencil.isSelected()) {
                 g.setFill(colorPicker.getValue());
                 g.fillRect(x, y, size, size);
+            }
+            if(radio_erase.isSelected()){
+                g.clearRect(x, y, size, size);
+            }
+        });
+
+        canvas.setOnMouseClicked(e -> {
+            if(radio_line.isSelected()) {
+                if (!flag) {
+                    flag = true;
+                    begin_x = e.getX();
+                    begin_y = e.getY();
+                } else {
+                    flag = false;
+                    end_x = e.getX();
+                    end_y = e.getY();
+                    g.strokeLine(begin_x, begin_y, end_x, end_y);
+                }
+            }
+
+            if(radio_rect.isSelected()) {
+                if (!flag) {
+                    flag = true;
+                    begin_x = e.getX();
+                    begin_y = e.getY();
+                } else {
+                    flag = false;
+                    end_x = e.getX();
+                    end_y = e.getY();
+                    g.strokeRect(begin_x, begin_y, end_x, end_y);
+                }
+            }
+
+            if(radio_oval.isSelected()) {
+                if (!flag) {
+                    flag = true;
+                    begin_x = e.getX();
+                    begin_y = e.getY();
+                } else {
+                    flag = false;
+                    end_x = e.getX();
+                    end_y = e.getY();
+                    g.strokeOval(begin_x, begin_y, end_x, end_y);
+                }
             }
         });
     }
