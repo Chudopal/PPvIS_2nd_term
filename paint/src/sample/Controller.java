b4503e185d18667e91cbbe361a8080fc7916ff05
chudopal <chudopal.00@mail.ru>
2020-02-17 13:01:08 +0300

    Added copy and paste

--- paint/src/sample/Controller.java
+++ paint/src/sample/Controller.java
@@ -7,7 +7,6 @@ package sample;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.util.Vector;
 
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
@@ -31,6 +30,10 @@ public class Controller {
     @FXML
     private Canvas real_canvas;
     @FXML
+    private Canvas backup_canvas;
+    @FXML
+    private Canvas copy_canvas;
+    @FXML
     private ColorPicker colorPicker;
     @FXML
     private TextField brushSize;
@@ -45,15 +48,16 @@ public class Controller {
     @FXML
     private RadioButton radio_oval;
     @FXML
-    private BorderPane borderPane;
+    private RadioButton radio_copy;
     @FXML
-    private Group root;
+    private RadioButton radio_paste;
 
     private double begin_x = 0;
     private double begin_y = 0;
     private double end_x = 0;
     private double end_y = 0;
     private boolean flag = false;
+    private boolean is_not_first_copy = false;
 
     public Controller() {
     }
@@ -62,6 +66,8 @@ public class Controller {
     public void initialize(){
         GraphicsContext g = canvas.getGraphicsContext2D();
         GraphicsContext real_g = real_canvas.getGraphicsContext2D();
+        GraphicsContext backup_g = backup_canvas.getGraphicsContext2D();
+        GraphicsContext copy_g = copy_canvas.getGraphicsContext2D();
 
         canvas.setOnMouseDragged(e -> {
 
@@ -79,8 +85,6 @@ public class Controller {
                 g.clearRect(x, y, size, size);
                 real_g.clearRect(x, y, size, size);
             }
-
-
 
             if(radio_line.isSelected()) {
                 if (!flag) {
@@ -106,14 +110,10 @@ public class Controller {
             if(radio_rect.isSelected()) {
                 if (!flag) {
 
-
-
                     flag = true;
                     begin_x = e.getX();
                     begin_y = e.getY();
                 } else {
-
-
                     end_x = e.getX();
                     end_y = e.getY();
 
@@ -125,18 +125,14 @@ public class Controller {
                     canvas.getGraphicsContext2D().drawImage(image, 0, 0);
 
                     if(begin_x < end_x && begin_y < end_y) {
-                        //g.clearRect(begin_x, begin_y, 600, 600);
                         g.strokeRect(begin_x, begin_y, end_x - begin_x, end_y - begin_y);
                     }
                     else if(begin_x > end_x && begin_y > end_y) {
-                        //g.clearRect(end_x, end_y, 600, 600);
                         g.strokeRect(end_x, end_y, Math.abs(end_x - begin_x), Math.abs(end_y - begin_y));
                     }else if(begin_x > end_x) {
-                        //g.clearRect(0, 0, 600, 600);
                         g.strokeRect(end_x, begin_y, Math.abs(end_x - begin_x), Math.abs(end_y - begin_y));
                     }
                     else if(begin_y > end_y) {
-                        //g.clearRect(0, 0, 600, 600);
                         g.strokeRect(begin_x, end_y, Math.abs(end_x - begin_x), Math.abs(end_y - begin_y));
                     }
                 }
@@ -160,32 +156,114 @@ public class Controller {
                     canvas.getGraphicsContext2D().drawImage(image, 0, 0);
 
                     if(begin_x < end_x && begin_y < end_y) {
-                        //g.clearRect(begin_x, begin_y, 600, 600);
                         g.strokeOval(begin_x, begin_y, end_x - begin_x, end_y - begin_y);
                     }
                     else if(begin_x > end_x && begin_y > end_y) {
-                        //g.clearRect(end_x, end_y, 600, 600);
                         g.strokeOval(end_x, end_y, Math.abs(end_x - begin_x), Math.abs(end_y - begin_y));
                     }else if(begin_x > end_x) {
-                        //g.clearRect(0, 0, 600, 600);
                         g.strokeOval(end_x, begin_y, Math.abs(end_x - begin_x), Math.abs(end_y - begin_y));
                     }
                     else if(begin_y > end_y) {
-                        //g.clearRect(0, 0, 600, 600);
                         g.strokeOval(begin_x, end_y, Math.abs(end_x - begin_x), Math.abs(end_y - begin_y));
                     }
                 }
             }
+
+            if(radio_copy.isSelected()) {
+                if (!flag) {
+                    flag = true;
+                    begin_x = e.getX();
+                    begin_y = e.getY();
+                } else {
+                    g.setStroke(Color.BLACK);
+                    g.setLineWidth(1);
+                    end_x = e.getX();
+                    end_y = e.getY();
+
+                    g.clearRect(0, 0, 600, 600);
+                    copy_g.clearRect(0, 0, 600, 600);
+
+                    SnapshotParameters params = new SnapshotParameters();
+                    params.setFill(Color.TRANSPARENT);
+                    WritableImage image = real_canvas.snapshot(params, null);
+                    canvas.getGraphicsContext2D().drawImage(image, 0, 0);
+
+                    if (!is_not_first_copy) {
+                        SnapshotParameters copy_params = new SnapshotParameters();
+                        copy_params.setFill(Color.TRANSPARENT);
+                        WritableImage copy_image = real_canvas.snapshot(copy_params, null);
+                        backup_canvas.getGraphicsContext2D().drawImage(copy_image, 0, 0);
+                    }else{
+                        real_g.clearRect(0,0,600,600);
+                        SnapshotParameters copy_params = new SnapshotParameters();
+                        copy_params.setFill(Color.TRANSPARENT);
+                        WritableImage copy_image = backup_canvas.snapshot(copy_params, null);
+                        real_canvas.getGraphicsContext2D().drawImage(copy_image, 0, 0);
+                    }
+
+                    is_not_first_copy = true;
+
+                    if (begin_x < end_x && begin_y < end_y) {
+                        g.strokeRect(begin_x, begin_y, end_x - begin_x, end_y - begin_y);
+                    } else if (begin_x > end_x && begin_y > end_y) {
+                        g.strokeRect(end_x, end_y, Math.abs(end_x - begin_x), Math.abs(end_y - begin_y));
+                    } else if (begin_x > end_x) {
+                        g.strokeRect(end_x, begin_y, Math.abs(end_x - begin_x), Math.abs(end_y - begin_y));
+                    } else if (begin_y > end_y) {
+                        g.strokeRect(begin_x, end_y, Math.abs(end_x - begin_x), Math.abs(end_y - begin_y));
+                    }
+
+                    SnapshotParameters copy_params = new SnapshotParameters();
+                    copy_params.setFill(Color.TRANSPARENT);
+                    WritableImage copy_image = real_canvas.snapshot(copy_params, null);
+                    copy_canvas.getGraphicsContext2D().drawImage(copy_image, -begin_x, -begin_y);
+
+                    copy_g.clearRect(end_x - begin_x, 0, 600, 600);
+                    copy_g.clearRect(0, end_y - begin_y, 600, 600);
+                }
+            }
+
         });
+
+
 
         canvas.setOnMousePressed(e -> {
             flag = false;
-            SnapshotParameters params = new SnapshotParameters();
-            params.setFill(Color.TRANSPARENT);
-            WritableImage image = canvas.snapshot(params, null);
-            real_canvas.getGraphicsContext2D().drawImage(image, 0, 0);
+
+            if(!radio_copy.isSelected() && is_not_first_copy){
+                real_g.clearRect(0,0,600,600);
+                g.clearRect(0,0,600,600);
+
+                SnapshotParameters backup_params = new SnapshotParameters();
+                backup_params.setFill(Color.TRANSPARENT);
+                WritableImage backup_image = backup_canvas.snapshot(backup_params, null);
+                real_canvas.getGraphicsContext2D().drawImage(backup_image, 0, 0);
+
+                SnapshotParameters copy_params = new SnapshotParameters();
+                copy_params.setFill(Color.TRANSPARENT);
+                WritableImage copy_image = real_canvas.snapshot(copy_params, null);
+                canvas.getGraphicsContext2D().drawImage(copy_image, 0, 0);
+
+                is_not_first_copy = false;
 
 
+            }else{
+                SnapshotParameters params = new SnapshotParameters();
+                params.setFill(Color.TRANSPARENT);
+                WritableImage image = canvas.snapshot(params, null);
+                real_canvas.getGraphicsContext2D().drawImage(image, 0, 0);
+            }
+
+            if (radio_paste.isSelected()){
+                double beg_x = e.getX();
+                double beg_y = e.getY();
+
+                SnapshotParameters cpy_params = new SnapshotParameters();
+                cpy_params.setFill(Color.TRANSPARENT);
+                WritableImage cpy_image = copy_canvas.snapshot(cpy_params, null);
+                canvas.getGraphicsContext2D().drawImage(cpy_image, beg_x, beg_y);
+
+            }
         });
     }
 
--- paint/src/sample/Main.java
+++ paint/src/sample/Main.java
@@ -7,13 +7,8 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Orientation;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 import javafx.fxml.FXML;
 
